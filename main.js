ab1a0288413c964dd7392609ed6fcc28b08f23ad
Kilbka68 <itspauerr@gmail.com>
2024-03-24 20:01:14 +0500

    added HTML

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ console.log('Hello World!')
 // git commit -m 'your message'
 // git branch -M main
 // git remote add origin Link
-// git push u origin main
+// git push -u origin main
 
 // git status
 
