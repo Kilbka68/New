c9e324f7ab81e0463abee712d1332d5f7502126b
Kilbka68 <itspauerr@gmail.com>
2024-03-24 19:44:59 +0500

    hz

---
+++ main.js
@@ -0,0 +1,18 @@
+// git config --global user.name "username"
+// git config --global user.mail "gmail"
+
+
+console.log('Hello World!')
+
+// git init
+// git add filename
+// git commit -m 'your message'
+// git branch -M main
+// git remote add origin Link
+// git push u origin main
+
+// git status
+
+// git clone Link
+
+// git 
